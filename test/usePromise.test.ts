3602c718a941b13a52aa42142e86a967449046f5
Dave Falke <dmfalke@gmail.com>
2020-09-30 08:44:25 -0400

    Add tests

---
+++ test/usePromise.test.ts
@@ -0,0 +1,43 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { usePromise } from '../src/hooks/usePromise';
+
+jest.useFakeTimers();
+
+function delay<T>(value: T, time: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time, value);
+  });
+}
+
+describe('usePromise', () => {
+
+  it('should handle resolve', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => usePromise(() => Promise.resolve(10)));
+    expect(result.current.pending).toBe(true);
+    await waitForNextUpdate();
+    expect(result.current.value).toBe(10);
+    expect(result.current.error).toBeUndefined();
+    expect(result.current.pending).toBe(false);
+  });
+
+  it('should handle reject', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => usePromise(() => Promise.reject('error')));
+    expect(result.current.pending).toBe(true);
+    await waitForNextUpdate();
+    expect(result.current.value).toBeUndefined();
+    expect(result.current.error).toBe('error');
+    expect(result.current.pending).toBe(false);
+  });
+
+  it('should ignore old promise', async () => {
+    const { result, waitForNextUpdate, rerender } = renderHook(({ callback }) => usePromise(callback), {
+      initialProps: { callback: () => delay('one', 20) }
+    });
+    jest.advanceTimersByTime(10);
+    rerender({ callback: () => delay('two', 30) });
+    jest.advanceTimersByTime(50);
+    await waitForNextUpdate();
+    expect(result.current.value).toBe('two');
+  });
+
+});
