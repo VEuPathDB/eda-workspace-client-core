ddf996554b955beec8f3872818267b88f583c6b7
Dave Falke <dmfalke@gmail.com>
2020-10-05 16:14:13 -0400

    fix import

--- src/hooks/useStudy.ts
+++ src/hooks/useStudy.ts
@@ -2,11 +2,11 @@ import { TypeOf } from "io-ts";
 import { useCallback } from "react";
 import { Study } from "../types/study";
 import { Store } from "../utils/store";
-import { PromiseHookReturn, usePromise } from "./usePromise";
+import { PromiseHookState, usePromise } from "./usePromise";
 
 export type Study = TypeOf<typeof Study>;
 export type StudyStore = Store<Study, 'get'>;
 
-export function useStudy(store: StudyStore, studyId: string): PromiseHookReturn<Study> {
+export function useStudy(store: StudyStore, studyId: string): PromiseHookState<Study> {
   return usePromise(useCallback(() => store.get(studyId), [ store, studyId]));
 }
