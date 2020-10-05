c764a5e461fb2f3f58480b6599106feb24d09ac7
Dave Falke <dmfalke@gmail.com>
2020-10-05 16:14:22 -0400

    add more tests

--- test/hooks/useAnalysis.test.ts
+++ test/hooks/useAnalysis.test.ts
@@ -19,16 +19,30 @@ beforeEach(() => {
 
 describe('useAnalysis', () => {
 
-  it('should load an analysis', async () => {
+  it('should have the correct status on success path', async () => {
     const { result, waitForValueToChange } = renderHook(() => useAnalysis(store, key));
-    await waitForValueToChange(() => result.current.analysis);
+    expect(result.current.status === 'in-progress');
+    await waitForValueToChange(() => result.current.status);
+    expect(result.current.status === 'loaded');
+  });
+
+  it('should have the correct status on failure path', async () => {
+    const { result, waitForValueToChange } = renderHook(() => useAnalysis(store, 'foo'));
+    expect(result.current.status === 'in-progress');
+    await waitForValueToChange(() => result.current.status);
+    expect(result.current.status === 'error');
+  });
+
+  it('should load an analysis', async () => {
+    const { result, waitFor } = renderHook(() => useAnalysis(store, key));
+    await waitFor(() => result.current.status === 'loaded');
     expect(result.current.analysis).toBeDefined();
     expect(result.current.analysis?.name).toBe('My Analysis');
   });
 
   it('should allow updates', async () => {
-    const { result, waitForValueToChange } = renderHook(() => useAnalysis(store, key));
-    await waitForValueToChange(() => result.current.analysis)
+    const { result, waitFor } = renderHook(() => useAnalysis(store, key));
+    await waitFor(() => result.current.status === 'loaded')
     act(() => {
       result.current.setName('New Name');
     });
@@ -36,13 +50,30 @@ describe('useAnalysis', () => {
   });
 
   it('should update store on save', async () => {
-    const { result, waitForValueToChange } = renderHook(() => useAnalysis(store, key));
-    await waitForValueToChange(() => result.current.analysis)
+    const { result, waitFor } = renderHook(() => useAnalysis(store, key));
+    await waitFor(() => result.current.status === 'loaded')
     act(() => result.current.setName('New Name'));
     expect(result.current.hasUnsavedChanges).toBeTruthy();
     await act(() => result.current.saveAnalysis());
     const analysis = await store.get(key);
     expect(analysis.name).toBe('New Name');
+    expect(result.current.hasUnsavedChanges).toBeFalsy();
+  });
+
+  it('should update store on copy', async () => {
+    const { result, waitFor } = renderHook(() => useAnalysis(store, key));
+    await waitFor(() => result.current.status === 'loaded');
+    const newId = await result.current.copyAnalysis();
+    const newAnalysis = await store.get(newId);
+    expect(result.current.analysis).toEqual(newAnalysis);
+    expect(result.current.analysis).not.toBe(newAnalysis);
+  });
+
+  it('should update store on delete', async () => {
+    const { result, waitFor } = renderHook(() => useAnalysis(store, key));
+    await waitFor(() => result.current.status === 'loaded');
+    await result.current.deleteAnalysis();
+    expect(store.get(key)).rejects.toThrow();
   });
 
 });
