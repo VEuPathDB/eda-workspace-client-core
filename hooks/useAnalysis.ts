85f5243e27a33457b07a014aa4706adb7fc6e65d
Dave Falke <dmfalke@gmail.com>
2020-10-13 13:58:54 -0400

    Migrate hook to use AnalysisApi

--- hooks/useAnalysis.ts
+++ hooks/useAnalysis.ts
@@ -1,7 +1,8 @@
-import localforage from 'localforage';
 import { useCallback, useEffect, useState } from "react";
 import { Analysis } from "../types/analysis";
 import { usePromise } from "./usePromise";
+import { AnalysisApi } from '../api/analysis-api';
+import { ApiRequestHandler } from 'ebrc-client/util/api';
 
 type Setter<T extends keyof Analysis> = (value: Analysis[T]) => void;
 
@@ -20,32 +21,16 @@ type Return = {
   saveAnalysis: () => Promise<void>;
 }
 
-export function useAnalysis(analysisId: string): Return {
+export function useAnalysis(analysisId: string, api: AnalysisApi, request: ApiRequestHandler): Return {
   const [analysis, setAnalysis] = useState<Analysis>();
   const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
-  const store = localforage.createInstance({
-    name: 'eda-workspace-analysis-store'
-  });
-  const savedAnalysis = usePromise(useCallback(async (): Promise<Analysis> => {
-    const item = await store.getItem<Analysis|null>(analysisId);
-    if (item != null) return item;
-    return {
-      name: 'Unsaved Analysis',
-      filters: [],
-      derivedVariables: [],
-      starredVariables: [],
-      visualizations: [],
-      variableUISettings: {},
-    }
-  }, [analysisId]));
+  const savedAnalysis = usePromise(useCallback((): Promise<Analysis> => {
+    return request(api.getAnalysis(analysisId));
+  }, [analysisId, api, request]));
 
   useEffect(() => {
     setAnalysis(savedAnalysis.value);
   }, [savedAnalysis.value]);
-
-  useEffect(() => {
-    store.setItem(analysisId, analysis);
-  }, [ analysisId, analysis ])
 
   const status = savedAnalysis.pending ? 'in-progress'
   : savedAnalysis.error ? 'error'
@@ -65,20 +50,20 @@ export function useAnalysis(analysisId: string): Return {
 
   const saveAnalysis = useCallback(async () => {
     if (analysis == null) throw new Error("Attempt to save an analysis that hasn't been loaded.");
-    await store.put(analysisId, analysis)
+    await request(api.updateAnalysis(analysis));
     setHasUnsavedChanges(false);
-  }, [store, analysisId, analysis])
+  }, [api, request, analysis])
 
   const copyAnalysis = useCallback(async () => {
     if (analysis == null) throw new Error("Attempt to copy an analysis that hasn't been loaded.");
     if (hasUnsavedChanges) await saveAnalysis();
-    const id = await store.post(analysis);
-    return id
-  }, [store, analysis, saveAnalysis, hasUnsavedChanges]);
+    const {id} = await request(api.createAnalysis(analysis));
+    return id;
+  }, [api, request, analysis, saveAnalysis, hasUnsavedChanges]);
 
   const deleteAnalysis = useCallback(async () => {
-    return store.delete(analysisId);
-  }, [store, analysisId]);
+    return request(api.deleteAnalysis(analysisId));
+  }, [api, request, analysisId]);
 
   return {
     status,
